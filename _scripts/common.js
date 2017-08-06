b71a857def18ae360ad87ef14de11e4c9e65942f
Sun1ive <sunliveua@gmail.com>
2017-08-06 23:20:18 +0300

    0.4 +vue test
    +some vue.js
    test

--- _scripts/common.js
+++ _scripts/common.js
@@ -1,6 +1,68 @@
+window.Event = new Vue();
+
+const About = { template: `<div class="about">
+	<h1>Hello there!</h1>
+		<h2>There is some info</h2>
+			<p>
+				<slot></slot>
+			</p>
+	</div>` 
+}
+const Blog = { template: `<div>
+	<h1>Hello there!</h1>
+	</div>` 
+}
+const routes = [
+	{ path: '/about-us', component: About },
+	{ path: '/blog', component: Blog }
+]
+
+
+const router = new VueRouter({
+	routes // сокращение от `routes: routes`
+})
+
+Vue.component('app-post',{
+	data(){
+	return { email: '' }
+	},
+	template: `
+	<form class="button-subscribe" id="app">
+	<input type="email" placeholder="Your Email Adress" v-model="email">
+	<button class="myButton" @click="subForm">Subscribe</button>
+	</form>
+`,
+	methods: {
+		subForm(){
+			Event.$emit('clicked', this.email);
+		}
+	}
+});
+
+// const app = new Vue({
+// 	router
+// }).$mount('#app')
 
 new Vue({
+	el: '#wrapper',
+	router,
+	data: {
+		show: false,
+		solMenu: false
+	},
+	created(){
+		Event.$on('clicked', function(email) {
+			axios.post('/users', {
+				userMail: this.email
+			})
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		})
+	}
+});
 
-	el: "#app"
 
-})
